9e18c6ce39dd079b7d76ac6bdcb4bc88e709d7b4
Justin Ralph <jr122019@protonmail.com>
2020-11-30 18:43:48 -0500

    User can add a profile picture. Username is displayed in profile view

--- Taste-Buds/ProfileViewController.swift
+++ Taste-Buds/ProfileViewController.swift
@@ -7,13 +7,96 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
-class ProfileViewController: UIViewController {
-
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    @IBOutlet weak var username: UILabel!
+    
+    
+    @IBOutlet weak var ProfilePic: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        ProfilePic.layer.cornerRadius = 75
+        let user = PFUser.current()
+        username.text = user?.username
+        //set profile pic from database
+        let imageFile = user!["ProfilePic"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        ProfilePic.af.setImage(withURL: url)
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onChangeProfilePic(_ sender: Any) {
+        let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallery()
+        }))
+
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    func openGallery()
+   {
+       if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+           let imagePicker = UIImagePickerController()
+           imagePicker.delegate = self
+           imagePicker.allowsEditing = true
+           imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+           self.present(imagePicker, animated: true, completion: nil)
+       }
+       else
+       {
+           let alert  = UIAlertController(title: "Warning", message: "You don't have permission to access gallery.", preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+           self.present(alert, animated: true, completion: nil)
+       }
+   }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 500, height: 50)
+        let scaledimage = image.af.imageAspectScaled(toFill: size)
+        ProfilePic.image = scaledimage
+        
+        //update profile pic on parse
+        let user = PFUser.current()
+        let imageData = ProfilePic.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        user?.setObject(file as Any, forKey: "ProfilePic")
+        ProfilePic.layer.cornerRadius = 60
+        user?.saveInBackground{ (success, error) in if success{
+            print("Saved!")
+            self.dismiss(animated: true, completion: nil)
+        }else{
+            print("Error!")
+        }
+            
+        }
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func onLogOut(_ sender: Any) {
